b38e1d9ec0cbf734a5c6197de76b2e8fe5662b83
Ankit Bansal <bansal.ankit@infoedge.com>
2020-07-14 16:38:09 +0530

    added method to copy link

--- web/src/components/Dialog.js
+++ web/src/components/Dialog.js
@@ -43,6 +43,17 @@ let dialogContent2 = {
 }
 
 class Dialog extends Component {
+   
+    copyToClipboard = (e) => {
+        var interviewLink = document.getElementById("interviewLink");
+	var textArea = document.createElement("textarea");
+        textArea.value = interviewLink.text;
+	document.body.appendChild(textArea);
+	textArea.select();
+        document.execCommand('copy');
+        textArea.remove();
+    };
+
     render() {
         let dialog = (
             <div style={dialogStyles}>
@@ -50,7 +61,7 @@ class Dialog extends Component {
                 <strong style={{ fontSize:'16px' }}> Schedule Interview : </strong>
                 <button style={dialogCloseButtonStyles} onClick={this.props.onClose}>x</button>
                 <table style={dialogContent1} width="100%" cellspacing="0" cellpadding="15">  <tbody><tr> <td> <img src="naukri_image.png" width="100" height="100" alt="Naukri Logo"/>  </td> </tr>  <tr> <td id="m_-8693798704384470617mailBodyData" style={dialogContent2}> Dear Candidate, <br/><br/>An interview is being scheduled for Requirement ABC               <ul>         <li>Interview On: 11th July 2020</li>                 <li>Location: At your desk
-</li>                 <li>Interview Type: Video interview </li>                            <li>Duration: 1 hour(s) and 0 mins</li><br/>                ------------------------------------------------------------------------------------------------    <br />    Join interview on - RMS WebRTC App <br/><a href={this.props.children} target="_blank">{this.props.children}</a>             
+</li>                 <li>Interview Type: Video interview </li>                            <li>Duration: 1 hour(s) and 0 mins</li><br/>                ------------------------------------------------------------------------------------------------    <br />    Join interview on - RMS WebRTC App <br/><a id="interviewLink" href={this.props.children} target="_blank">{this.props.children}</a>  <button style={{marginLeft: '20px'}} type="button" onClick={this.copyToClipboard}>Copy</button>              
 		<br/>------------------------------------------------------------------------------------------------<br/>                 </ul><br/> Please find the candidates details attached with the mail.<br/><br/>                 Thanks and Regards <br/>                
 <a href="mailto:surrya@naukri.com" target="_blank">surrya.@naukri.com</a> </td> </tr>  </tbody></table>
                
